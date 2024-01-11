8df145c19730ac219f6c8cfa68e7a0a292470afd
mackenzie kormann <mackenziekormann@mackenzies-air.lan>
2024-01-11 18:27:39 -0500

    updates to the main file

--- go.mod
+++ go.mod
@@ -8,12 +8,10 @@ require (
 )
 
 require (
-	github.com/chzyer/readline v1.5.1 // indirect
 	github.com/go-logr/logr v1.4.1 // indirect
 	github.com/go-task/slim-sprig v0.0.0-20230315185526-52ccab3ef572 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/google/pprof v0.0.0-20231229205709-960ae82b1e42 // indirect
-	github.com/ianlancetaylor/demangle v0.0.0-20230524184225-eabc099b10ab // indirect
 	golang.org/x/net v0.20.0 // indirect
 	golang.org/x/sys v0.16.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,40 @@
 package main
 
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/mackenziekormann/biodata/pkg/alphafold"
+)
+
 func main() {
-	
+	http.HandleFunc("/fetchProteinData", func(w http.ResponseWriter, r *http.Request) {
+		// Assume the protein ID is passed as a query parameter
+		proteinID := r.URL.Query().Get("proteinID")
+		if proteinID == "" {
+			http.Error(w, "Protein ID is required", http.StatusBadRequest)
+			return
+		}
+
+		data, err := alphafold.FetchAlphaFoldData(proteinID)
+		if err != nil {
+			log.Println("Error fetching protein data:", err)
+			http.Error(w, "Error fetching protein data", http.StatusInternalServerError)
+			return
+		}
+
+		// Respond with the fetched data
+		w.Header().Set("Content-Type", "application/json")
+		if err := json.NewEncoder(w).Encode(data); err != nil {
+			log.Println("Error encoding response:", err)
+			http.Error(w, "Error encoding response", http.StatusInternalServerError)
+		}
+	})
+
+	// Start the server
+	log.Println("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
 }
